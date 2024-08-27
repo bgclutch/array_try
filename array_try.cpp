0b4f73749836378d84efca00a60a0bca4c40e536
Bogdan <bvolsky2006@gmail.com>
2024-08-27 17:40:02 +0300

    edited dynamic array

--- array_try.cpp
+++ array_try.cpp
@@ -1,36 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void array_print(int *arr, const unsigned int size_x, const unsigned int size_y);
 
 int main(void)
 {
-    /*
-    printf("input size of your array:\n\n");
-    int ar_size = 0;
-    scanf("%d", &ar_size);
-    */
-    const int SIZE_X = 5;
-    const int SIZE_Y = 3;
-    int array[SIZE_Y][SIZE_X] = {{1, 11, 121, 131, 141}, //array initialization 
-                                 {2, 22, 212, 232, 242},
-                                 {3, 33, 313, 313, 343}};
+    printf("input size of your array[rows][cols]:\n\n");
+    unsigned int size_x = 0;
+    unsigned int size_y = 0;
+    scanf("%u %u", &size_x, &size_y);
+    const unsigned int main_size = size_x * size_y;
+    int *arr;
+    arr = (int*) malloc(main_size * sizeof(double));
+
+    printf("input your array's elements:\n");
+    // while()
+    // {
+
+    // }
+    for(unsigned int index = 0; index < main_size; index++)
+    {
+        scanf("%d ", &arr[index]);
+    }
+
     printf("\n");
-    //int (*arr)[SIZE_X];
-    //arr = array;
-    array_print(*array, SIZE_X, SIZE_Y);
+    array_print(arr, size_x, size_y);
+
+    free(arr);
 
     return 0;
 }
 
 void array_print(int *arr, const unsigned int size_x, const unsigned int size_y)
 {
-    for(unsigned int index_y = 0; index_y < size_y; index_y++)
+    for(unsigned int index_x = 0; index_x < size_x; index_x++)
     {
-        for(unsigned int index_x = 0; index_x < size_x; index_x++)
-        { 
-            printf("address = %p\n", (int*)(size_t)arr + index_y * size_x + index_x);                                // address changes dex size to hex address
-            printf("element %u %u = %u\n\n", index_y, index_x, *((unsigned int*)arr + index_y * size_x + index_x));  // element
+        for(unsigned int index_y = 0; index_y < size_y; index_y++)
+        {                         
+            printf ("%u ", *((unsigned int*)arr + index_x * size_y + index_y));
         }
-        printf("\n\n\n");
+        printf("\n");
     }
 }
