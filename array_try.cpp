d9a0287975fc8bc9ce92f12d8dba3274f6b623f4
Bogdan <bvolsky2006@gmail.com>
2024-08-27 16:03:21 +0300

    output table array

--- array_try.cpp
+++ array_try.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void array_print(int *arr, const unsigned int size_x);
+void array_print(int *arr, const unsigned int size_x, const unsigned int size_y);
 
 int main(void)
 {
@@ -10,21 +10,27 @@ int main(void)
     scanf("%d", &ar_size);
     */
     const int SIZE_X = 5;
-    //const int SIZE_Y = 3;
-    int array[SIZE_X] = {1, 11, 121, 131, 141}; //array initialization 
+    const int SIZE_Y = 3;
+    int array[SIZE_Y][SIZE_X] = {{1, 11, 121, 131, 141}, //array initialization 
+                                 {2, 22, 212, 232, 242},
+                                 {3, 33, 313, 313, 343}};
     printf("\n");
-    array_print(array, SIZE_X);
+    //int (*arr)[SIZE_X];
+    //arr = array;
+    array_print(*array, SIZE_X, SIZE_Y);
 
     return 0;
 }
 
-void array_print(int *arr, const unsigned int size_x)
+void array_print(int *arr, const unsigned int size_x, const unsigned int size_y)
 {
-    for(unsigned int index = 0; index < size_x; index++)
+    for(unsigned int index_y = 0; index_y < size_y; index_y++)
     {
-        printf("%u element's address    = %p\n", index, arr + index);                      // address
-        printf("size_t + index         = %lu\n", (size_t)arr + index);                     // size of element's address in bytes 
-        printf("address                = %p\n", (int*)(size_t)arr + index);                // address changes dex size to hex address
-        printf("element                = %u\n\n", *((unsigned int*)(size_t)arr + index));  // element
+        for(unsigned int index_x = 0; index_x < size_x; index_x++)
+        { 
+            printf("address = %p\n", (int*)(size_t)arr + index_y * size_x + index_x);                                // address changes dex size to hex address
+            printf("element %u %u = %u\n\n", index_y, index_x, *((unsigned int*)arr + index_y * size_x + index_x));  // element
+        }
+        printf("\n\n\n");
     }
 }
