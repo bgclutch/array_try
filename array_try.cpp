cf9161e2548f52f135c4c864ae704821295f5167
Bogdan <bvolsky2006@gmail.com>
2024-08-27 14:07:53 +0300

    table arr

--- array_try.cpp
+++ array_try.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void array_print(int *arr, const int size);
+void array_print(int *arr, const int size_x, const int size_y);
 
 int main(void)
 {
@@ -10,14 +10,17 @@ int main(void)
     scanf("%d", &ar_size);
     */
     const int SIZE_X = 5;
-    //const int SIZE_Y = 7;
-    int array[SIZE] = {0, 1, 2, 3, 4}; //array initialization 
-    array_print(array, SIZE);
+    const int SIZE_Y = 3;
+    int array[SIZE_Y][SIZE_X] = {{10, 11, 12, 13, 14},
+                                 {20, 21, 22, 23, 24},
+                                 {30, 31, 32, 33, 34}}; //array initialization 
+
+    array_print(array, SIZE_Y, SIZE_X);
 
     return 0;
 }
 
-void array_print(int *arr, const int size)
+void array_print(int *arr, const int size_x, const int size_y)
 {
     for(int index = 0; index < size; index++)
         printf("array[%d] = %d\n", index, *(arr+index));
