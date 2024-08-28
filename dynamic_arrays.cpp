31eb9581d9a8c055a267f6db91f39ff09e9061c8
Bogdan <bvolsky2006@gmail.com>
2024-08-28 18:50:47 +0300

    scary scary

--- dynamic_arrays.cpp
+++ dynamic_arrays.cpp
@@ -3,37 +3,60 @@
 #include <stdlib.h>
 #include <assert.h>
 
-void array_print(int arr[], const int size_x, const int size_y);
-
 int main(void)
 {
     printf("input number of lines:\n");
 
-    int rows = 0;
-    scanf("%d", &rows);
+    int dynamic_rows = 0;
+    scanf("%d", &dynamic_rows);
+    printf("\n");
 
-    int *arrays_size = (int *) calloc(rows, sizeof(int));
+    int *arrays_sizes = (int *) calloc((unsigned int)dynamic_rows, sizeof(int));
 
-    //int *arrays_ptr = (int *) calloc(rows, sizeof(int));
-    
-    for(int i = 0; i < rows; i++)
+    int **arrays_ptrs  = (int **) calloc((unsigned int)dynamic_rows, sizeof(int*));
+
+    printf("sizes = %p, ptrs = %p\n\n", arrays_sizes, arrays_ptrs);
+
+    for(int i = 0; i < dynamic_rows; i++)
     {
-        printf("input len if your %d line", i);
-        scanf("%d ", (arrays_size+ i));
+        printf("input len if your %d line:\n", i + 1);
+        scanf("%d", (arrays_sizes + i));
+        *(arrays_ptrs + i) = (int *) calloc((unsigned int)*(arrays_sizes + i), sizeof(int));
+        printf("%p\n\n", arrays_ptrs);
     }
 
-    return 0;
-}
-
-
-void array_print(int arr[], const int size_x, const int size_y)
-{   
-    for(int index_x = 0; index_x < size_x ; index_x++)
+    for(int ar_ptr = 0; ar_ptr < dynamic_rows; ar_ptr++)
     {
-        for(int index_y = 0; index_y < size_y; index_y++)
-        {                         
-            printf ("%3u ", *((unsigned int*)arr + index_x * size_y + index_y));
+        for(int dif_size = 0; dif_size < *(arrays_sizes + ar_ptr); dif_size++)
+        {
+            printf("input %d element in %d line", dif_size + 1, ar_ptr + 1);
+            scanf("%d", ((*(arrays_ptrs) + dif_size) + ar_ptr));
+        }
+    }
+
+
+    for(int )
+    {
+        for()
+        {
+
         }
         printf("\n");
     }
+
+
+
+    for(int i = 0; i < dynamic_rows; i++)
+    {
+        free(*(arrays_ptrs + i));
+        *(arrays_ptrs + i) = NULL;
+    }
+
+    free(arrays_sizes);
+    arrays_sizes = NULL;
+
+    free(arrays_ptrs);
+    arrays_ptrs = NULL;
+
+    return 0;
 }
